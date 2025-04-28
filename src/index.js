0c0e8134c417fe86d9a64539dd81ce0a4dc9861f
Aram Sajdak <aram.sajdak@gmail.com>
2025-04-28 21:39:58 +0200

    add trailing / for redirect

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_dKOOlZhgj",
   client_id: "20ubm7idpsrksou3mf06nvl77r",
-  redirect_uri: "https://main.d5si462hr64b5.amplifyapp.com",
+  redirect_uri: "https://main.d5si462hr64b5.amplifyapp.com/",
   response_type: "code",
   scope: "email openid phone",
 };
